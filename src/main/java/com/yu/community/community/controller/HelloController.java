37edb4ea683ff59d229ba438b3509f56373cc861
yuehui <949274226@qq.com>
2019-10-14 12:19:08 +0800

    init repore

---
+++ src/main/java/com/yu/community/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.yu.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author yu
+ * @Description
+ * @create 2019-10-13-21:09
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
