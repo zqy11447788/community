f13b78b64d2688ca4d50b89833c2ed8151aa6c94
yuehui <949274226@qq.com>
2019-10-19 10:19:20 +0800

    profile

--- src/main/java/com/yu/community/dto/PaginationDTO.java
+++ src/main/java/com/yu/community/dto/PaginationDTO.java
@@ -23,20 +23,11 @@ public class PaginationDTO {
     private Integer page;
     private List<Integer> pages = new ArrayList<>();
 
-    public void setPaginAtion(Integer totalCount, Integer page, Integer size) {
+    public void setPaginAtion(Integer totalPage, Integer page) {
 
-        if (totalCount % size == 0){
-            totalPage = totalCount / size;
-        }else {
-            totalPage = (totalCount / size) +1;
-        }
-        if( page < 1 ){
-            page = 1;
-        }
-        if( page > totalPage){
-            page = totalPage;
-        }
+        this.totalPage = totalPage;
         this.page = page;
+
         pages.add(page);
         for (int i = 1; i <= 3; i++) {
             if(page - i >0){
