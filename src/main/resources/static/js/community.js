94f1a080972ee9db68dd9f554c9fee6186d5e53b
yuehui <949274226@qq.com>
2019-10-22 21:47:39 +0800

    question1

---
+++ src/main/resources/static/js/community.js
@@ -0,0 +1,25 @@
+function post(){
+    var questionId = $("#question_id").val();
+    var content = $("#comment_content").val();
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType:'application/json',
+        data: JSON.stringify({
+            "parentId":questionId,
+            "content":content,
+            "type":130
+        }),
+        success: function (response) {
+            if(response.code == 200){
+                $("#comment_section").hide();
+            }else{
+                alert(response.message);
+            }
+            console.log(response);
+        },
+        dataType: "json"
+    });
+    console.log(questionId);
+    console.log(content);
+}
